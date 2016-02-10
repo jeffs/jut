ee164bd9caa080aa03cbc8abf3c41761f183b46d
Jeff Schwab <jeff@orchardplatform.com>
2016-02-10 08:49:37 -0500

    Continue development of `either`.
    Begin testing in earnest.  Add namespace-scope `make_left` and
    `make_right`.

--- src/app/test-either.cpp
+++ src/app/test-either.cpp
@@ -4,33 +4,240 @@
 #include <string>
 
 using jut::dat::either;
+using std::move;
+using std::string;
+
+struct copy_report {
+    int copy_constructions = 0;
+    int copy_assignments   = 0;
+    int move_constructions = 0;
+    int move_assignments   = 0;
+
+    void clear() { *this = copy_report{}; }
+};
+
+struct copy_counter {
+
+    int          value;
+    copy_report& report;
+
+    copy_counter(int value_, copy_report& report_):
+        value(value_),
+        report(report_) { }
+
+    copy_counter(copy_counter const& rhs):
+        value(rhs.value),
+        report(rhs.report) {
+        ++report.copy_constructions;
+    }
+
+    copy_counter& operator=(copy_counter const& rhs) & {
+        assert(&report == &rhs.report);
+        value = rhs.value;
+        ++report.move_constructions;
+        return *this;
+    }
+
+    copy_counter(copy_counter&& rhs):
+        value(rhs.value),
+        report(rhs.report) {
+        ++report.move_constructions;
+    }
+
+    copy_counter& operator=(copy_counter&& rhs) & {
+        assert(&report == &rhs.report);
+        value = move(rhs.value);
+        ++report.move_assignments;
+        return *this;
+    }
+};
+
+void test_make_left() {
+    auto x = either<int, string>::make_left(42);
+    assert(x.is_left());
+    assert(x.left() == 42);
+}
+
+void test_make_right() {
+    auto x = either<int, string>::make_right("hello");
+    assert(x.is_right());
+    assert(x.right() == "hello");
+}
+
+void test_default_constructor() {
+    either<int, string> x;
+    assert(x.is_left());
+    assert(x.left() == 0);
+}
+
+void test_copy_constructor_left() {
+    auto y = either<int, string>::make_left(42), x = y;
+    assert(x.is_left());
+    assert(x.left() == 42);
+}
+
+void test_copy_constructor_right() {
+    auto y = either<int, string>::make_right("hello"), x = y;
+    assert(x.is_right());
+    assert(x.right() == "hello");
+}
+
+void test_copy_assignment_left_left() {
+    auto x = either<int, string>::make_left(1);
+    auto y = either<int, string>::make_left(2);
+    x = y;
+    assert(x.is_left());
+    assert(x.left() == 2);
+}
+
+void test_copy_assignment_left_right() {
+    auto x = either<int, string>::make_left(42);
+    auto y = either<int, string>::make_right("hello");
+    x = y;
+    assert(x.is_right());
+    assert(x.right() == "hello");
+}
+
+void test_copy_assignment_right_left() {
+    auto x = either<int, string>::make_right("hello");
+    auto y = either<int, string>::make_left(42);
+    x = y;
+    assert(x.is_left());
+    assert(x.left() == 42);
+}
+
+void test_copy_assignment_right_right() {
+    auto x = either<int, string>::make_right("hello");
+    auto y = either<int, string>::make_right("world");
+    x = y;
+    assert(x.is_right());
+    assert(x.right() == "world");
+}
+
+void test_move_constructor_left() {
+    copy_report r;
+    copy_counter c(42, r);
+    auto y = either<copy_counter, string>::make_left(c);
+    r.clear();
+    either<copy_counter, string> x = move(y);
+    assert(x.is_left());
+    assert(x.left().value == 42);
+    assert(r.copy_constructions == 0);
+    assert(r.copy_assignments   == 0);
+    assert(r.move_constructions == 1);
+    assert(r.move_assignments   == 0);
+}
+
+void test_move_constructor_right() {
+    copy_report r;
+    copy_counter c(42, r);
+    auto y = either<string, copy_counter>::make_right(c);
+    r.clear();
+    either<string, copy_counter> x = move(y);
+    assert(x.is_right());
+    assert(x.right().value == 42);
+    assert(r.copy_constructions == 0);
+    assert(r.copy_assignments   == 0);
+    assert(r.move_constructions == 1);
+    assert(r.move_assignments   == 0);
+}
+
+void test_move_assignment_left_left() {
+    copy_report r;
+    copy_counter c1(1, r), c2(2, r);
+    auto x = either<copy_counter, string>::make_left(c1);
+    auto y = either<copy_counter, string>::make_left(c2);
+    r.clear();
+    x = move(y);
+    assert(x.is_left());
+    assert(x.left().value == 2);
+    assert(r.copy_constructions == 0);
+    assert(r.copy_assignments   == 0);
+    assert(r.move_constructions == 0);
+    assert(r.move_assignments   == 1);
+}
+
+void test_move_assignment_left_right() {
+    copy_report r;
+    copy_counter c(42, r);
+    auto x = either<string, copy_counter>::make_left("hello");
+    auto y = either<string, copy_counter>::make_right(c);
+    r.clear();
+    x = move(y);
+    assert(x.is_right());
+    assert(x.right().value == 42);
+    assert(r.copy_constructions == 0);
+    assert(r.copy_assignments   == 0);
+    assert(r.move_constructions == 1);
+    assert(r.move_assignments   == 0);
+}
+
+void test_move_assignment_right_left() {
+    copy_report r;
+    copy_counter c(42, r);
+    auto x = either<copy_counter, string>::make_right("hello");
+    auto y = either<copy_counter, string>::make_left(c);
+    r.clear();
+    x = move(y);
+    assert(x.is_left());
+    assert(x.left().value == 42);
+    assert(r.copy_constructions == 0);
+    assert(r.copy_assignments   == 0);
+    assert(r.move_constructions == 1);
+    assert(r.move_assignments   == 0);
+}
+
+void test_move_assignment_right_right() {
+    copy_report r;
+    copy_counter c1(1, r), c2(2, r);
+    auto x = either<string, copy_counter>::make_right(c1);
+    auto y = either<string, copy_counter>::make_right(c2);
+    r.clear();
+    x = move(y);
+    assert(x.is_right());
+    assert(x.right().value == 2);
+    assert(r.copy_constructions == 0);
+    assert(r.copy_assignments   == 0);
+    assert(r.move_constructions == 0);
+    assert(r.move_assignments   == 1);
+}
 
 int main() {
 
-    // can we CONTSRUCT an either
-    //  - left from lvalue
-    //  - right from lvalue
-    //  - left from rvalue
-    //  - right from rvalue
+    test_make_left();
+    test_make_right();
+    test_default_constructor();
+    test_copy_constructor_left();
+    test_copy_constructor_right();
+    test_copy_assignment_left_left();
+    test_copy_assignment_left_right();
+    test_copy_assignment_right_left();
+    test_copy_assignment_right_right();
+    test_move_constructor_left();
+    test_move_constructor_right();
+    test_move_assignment_left_left();
+    test_move_assignment_left_right();
+    test_move_assignment_right_left();
+    test_move_assignment_right_right();
 
-    // make_left(L const& value);
-    // make_right(R const& value);
+    // DESTROY
+    //  - destroy left
+    //  - destroy right
+    //  - destroy left after move-from
+    //  - destroy right after move-from
 
-    // either(either const& rhs);
-    // either(either&& rhs);
+    // MANIPULATE
+    //  - left on rvalue
+    //  - right on rvalue
+    //  - leftp
+    //  - rightp
 
-    // ~either();
-    // operator=(either const& rhs);
-    // operator=(either&& rhs);
+    // ACCESS
+    //  - is_left on const
+    //  - is_right on const
+    //  - left on const
+    //  - right on const
+    //  - leftp on const
+    //  - rightp on const
 
-    // is_left() const;
-    // is_right() const;
-    // left() const;
-    // right() const;
-    // leftp() const;
-    // rightp() const;
-
-    auto left = either<int, std::string>::make_left(42);
-    assert(left.is_left());
-    assert(left.left() == 42);
 }
--- src/jut/dat/either.hpp
+++ src/jut/dat/either.hpp
@@ -1,4 +1,7 @@
-/** @file jut/dat/either.hpp Algebraic type having two branches. */
+/** @file jut/dat/either.hpp Generic variant type having two branches.
+ *
+ * @todo allocator awareness
+ */
 
 #ifndef JUT_DAT_EITHER_INCLUDED
 #define JUT_DAT_EITHER_INCLUDED
@@ -14,63 +17,93 @@ namespace dat { // DATa structures
 using std::false_type;
 using std::true_type;
 
-template<class L, class R>  // Left, Right
+template <class T> struct left_t { T value; };
+template <class T> struct right_t { T value; };
+
+/** Holds either an `L` or an `R` at any given time. */
+template <class L, class R>  // Left, Right
 class either {
 
-    // PRIVATE FIELDS
+    // PRIVATE NON-STATIC DATA MEMBERS
     union { L _left; R _right; };
     bool _is_right;
 
-    // PRIVATE METHODS
+    // PRIVATE NON-STATIC MEMBER FUNCTIONS
     either(false_type, L value): _left(std::move(value)), _is_right(false) { }
     either(true_type, R value): _right(std::move(value)), _is_right(true) { }
 
   public:
 
-    // PUBLIC STATIC METHODS
+    // PUBLIC STATIC MEMBER FUNCTIONS
 
-    template<class L1, class =std::enable_if<std::is_same<L, L1>{}>>
-    static either make_left(L1&& value) {
-        return {false_type{}, std::forward<L1>(value)};
-    }
+    static
+    either make_left(L value) { return {false_type{}, std::move(value)}; }
 
-    template<class R1, class =std::enable_if<std::is_same<R, R1>{}>>
-    static either make_right(R1&& value) {
-        return {true_type{}, std::forward<R1>(value)};
-    }
+    static
+    either make_right(R value) { return {true_type{}, std::move(value)}; }
 
     // PUBLIC METHODS
 
-    // Special member functions
+    // Special member functions, ordered as in [special]: default constructor,
+    // copy constructor and copy assignment operator, move constructor and move
+    // assignment operator, and destructor.
 
-    template<class E, class =std::enable_if_t<std::is_same<E, either>{}>>
-    either(E&& rhs):
+    either(): _left(), _is_right(false) { }
+
+    either(either const& rhs):
         _is_right(rhs._is_right) {
-        if (is_left()) new (&_left)  L(std::forward<E>(rhs)._left);
-        else           new (&_right) R(std::forward<E>(rhs)._right);
+        if (is_left()) new (&_left)  L(rhs._left);
+        else           new (&_right) R(rhs._right);
     }
 
-    template<class E, class =std::enable_if_t<std::is_same<E, either>{}>>
-    either& operator=(E&& rhs) & {
+    either& operator=(either const& rhs) & {
         if (is_left()) {
             if (rhs.is_left()) {
-                _left = std::forward<E>(rhs)._left;
+                _left = rhs._left;
             } else {
                 _left.~L();
-                new (&_right) R(std::forward<R>(rhs)._right);
+                new (&_right) R(rhs._right);
             }
         } else if (rhs.is_left()) {
             _right.~R();
-            new (&_left) L(std::forward<E>(rhs)._left);
+            new (&_left) L(rhs._left);
         } else {
-            _right = std::forward<E>(rhs)._right;
+            _right = rhs._right;
         }
+        _is_right = rhs._is_right;
         return *this;
     }
 
-    ~either() { if (is_left()) _left.~L(); else _right.~R(); }
+    either(either&& rhs):
+        _is_right(rhs._is_right) {
+        if (is_left()) new (&_left)  L(std::move(rhs._left));
+        else           new (&_right) R(std::move(rhs._right));
+    }
 
-    // Manipulators
+    either& operator=(either&& rhs) & {
+        if (is_left()) {
+            if (rhs.is_left()) {
+                _left = std::move(rhs._left);
+            } else {
+                _left.~L();
+                new (&_right) R(std::move(rhs._right));
+            }
+        } else if (rhs.is_left()) {
+            _right.~R();
+            new (&_left) L(std::move(rhs._left));
+        } else {
+            _right = std::move(rhs._right);
+        }
+        _is_right = rhs._is_right;
+        return *this;
+    }
+
+    ~either() {
+        if (is_left()) _left.~L();
+        else           _right.~R();
+    }
+
+    // non-const
 
     L&& left() && { assert(is_left()); return std::move(_left); }
     R&& right() && { assert(is_right()); return std::move(_right); }
@@ -78,7 +111,7 @@ class either {
     L* leftp() { return is_left() ? &_left : nullptr; }
     R* rightp() { return is_right() ? &_right : nullptr; }
 
-    // Accessors
+    // const
 
     bool is_left() const { return !_is_right; }
     bool is_right() const { return _is_right; }
@@ -90,6 +123,12 @@ class either {
     R const* rightp() const { return is_right() ? &_right : nullptr; }
 
 };
+
+template <class T>
+left_t<T> make_left(T&& value) { return {std::forward<T>(value)}; }
+
+template <class T>
+right_t<T> make_right(T&& value) { return {std::forward<T>(value)}; }
 
 }   // dat
 }   // jut
