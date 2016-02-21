06a3725ea3e147938b5cc9cb89a06125ab01aa90
Jeff Schwab <jeff@orchardplatform.com>
2016-02-21 10:57:53 -0500

    Add jut/data/slowcat component and app/slowcat.

---
+++ src/jut/dat/slowcat.hpp
@@ -0,0 +1,35 @@
+/** @file jut/dat/slowcat.hpp Slowly copies data from one stream to another.
+ *
+ * @todo Async.
+ */
+
+#ifndef JUT_DAT_SLOWCAT_INCLUDED
+#define JUT_DAT_SLOWCAT_INCLUDED
+
+// C++ standard
+#include <chrono>
+#include <iosfwd>
+
+namespace jut { // Jeff's UTilities
+namespace dat { // DATa structures and manipulation
+
+std::chrono::milliseconds const slowcat_delay_default(50);
+
+/** Copies `input` to `output`, sleeping `period` before writing each byte. */
+void slowcat(
+        std::istream&             input,
+        std::ostream&             output,
+        std::chrono::milliseconds period =slowcat_delay_default);
+
+/** Delegates to lvalue overload. */
+inline void slowcat(
+        std::istream&&            input,
+        std::ostream&             output,
+        std::chrono::milliseconds period =slowcat_delay_default) {
+    slowcat(input, output, period);
+}
+
+}   // dat
+}   // jut
+
+#endif
