64c1319ba6ed42b9ebad70a0866b36cd98ce93f5
Jeff Schwab <jeff@orchardplatform.com>
2016-11-15 20:23:21 -0500

    Read input.  Skip blank lines and comments.

--- src/app/nutrition.cpp
+++ src/app/nutrition.cpp
@@ -1,6 +1,8 @@
 #include "jut/err/site.hpp"
+#include "jut/txt/trim.hpp"
 
 #include <ctime>
+#include <fstream>
 #include <iostream>
 
 static std::string date_stamp() {
@@ -10,6 +12,34 @@ static std::string date_stamp() {
     return buffer;
 }
 
-int main(int argc, char** argv) {
-    std::cout << date_stamp() << '\n';
+static std::string strip_comment(std::string const& line) {
+    auto e = line.find('#');
+    return e == std::string::npos ? line : line.substr(0, e);
+}
+
+static bool get_line(std::istream& in, std::string& line) {
+    while (std::getline(in, line)) {
+        line = jut::txt::trim(strip_comment(line));
+        if (!line.empty())
+            return true;
+    }
+    return false;
+}
+
+int main(int argc, char** argv) try {
+    std::string const path = "/Users/jeff/home/fit/log/" + date_stamp();
+    std::ifstream in(path);
+    if (!in)
+        throw "can't read file: " + path;
+    for (std::string line; get_line(in, line);)
+        std::cout << line << '\n';
+} catch (char const* what) {
+    std::clog << "error: " << what << '\n';
+    return 1;
+} catch (std::string const& what) {
+    std::clog << "error: " << what << '\n';
+    return 1;
+} catch (std::runtime_error const& error) {
+    std::clog << "error: " << error.what() << '\n';
+    return 2;
 }
